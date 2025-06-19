02126bc5d65e5a83ad99af7d638e2f5a89e4002b
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-20 01:48:34 +0600

    email validation

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -2,15 +2,35 @@ import { model, Schema } from "mongoose";
 
 
 const userSchema = new Schema<IUser>({
-name:{type: String, required: true},
-email:{type: String, required: true},
-phone: {type: String, required: true},
-password: {type: String, required: true},
-role:{
-    type: String,
-    enum:["Admin", "Customer"],
-    required: true
-}
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate:{
+            validator: function(value){
+                return  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+            },
+            message: props=> `${props.value} is not valid email `
+        }
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["Admin", "Customer"],
+        required: true
+    }
 })
 
 const User = model<IUser>("User", userSchema)
