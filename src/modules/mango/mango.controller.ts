c5bb9b4b8e9062534b23665dfa7743459333bc7c
Khandaker-Mohyet <mohyet008@gmail.com>
2025-06-20 14:40:00 +0600

    update and delete Mango

--- src/modules/mango/mango.controller.ts
+++ src/modules/mango/mango.controller.ts
@@ -27,3 +27,43 @@ export const getMango = async(req:Request, res: Response) =>{
         data
     })
 }
+
+
+export const getMangoById = async(req:Request, res: Response) =>{
+    const id = req.params.id
+
+    const data = await Mango.findById(id)
+
+    res.send({
+        success : true,
+        message: "User created Successfully",
+        data
+    })
+}
+
+
+export const deleteMango = async(req:Request, res: Response) =>{
+    const id = req.params.id
+
+    const data = await Mango.findByIdAndDelete(id,)
+
+    res.send({
+        success : true,
+        message: "User update Successfully",
+        data
+    })
+}
+
+
+export const updateMango = async(req:Request, res: Response) =>{
+    const id = req.params.id
+    const body = req.body
+
+    const data = await Mango.findByIdAndUpdate(id, body, {new:true, runValidators: true})
+
+    res.send({
+        success : true,
+        message: "User update Successfully",
+        data
+    })
+}
--- src/modules/mango/mango.route.ts
+++ src/modules/mango/mango.route.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { createMango, getMango } from "./mango.controller";
+import { createMango, deleteMango, getMango, getMangoById, updateMango } from "./mango.controller";
 
 
 export const mangoRouter = Router()
 
 mangoRouter.post("/create-mango", createMango)
 mangoRouter.get("/get-mango", getMango)
+mangoRouter.get("/get-mango/:id", getMangoById)
+mangoRouter.delete("/delete-mango/:id", deleteMango)
+mangoRouter.put("/update-mango/:id", updateMango)
